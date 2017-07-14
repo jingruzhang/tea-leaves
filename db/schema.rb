890ac6f94fa2dec0e86d1d9bb73f22cfb9ad0694
JingruZhang <zhang.jingru.93@gmail.com>
2017-07-13 19:36:10 -0700

    add admin and set views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712004713) do
+ActiveRecord::Schema.define(version: 20170714013814) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "content"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170712004713) do
     t.string "name"
     t.string "provider"
     t.string "uid"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
     t.index ["provider"], name: "index_users_on_provider"
