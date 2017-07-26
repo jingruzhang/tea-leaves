c0abc971fb7679edb31a8b7c76fd1172b0502b88
JingruZhang <zhang.jingru.93@gmail.com>
2017-07-25 17:55:10 -0700

    typo

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -250,7 +250,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, ENV['APP_ID'], ENV['APP_SECRET'],
+  config.omniauth :facebook, ENV['APP_ID'], ENV['FACEBOOK_SECRET'],
                   callback_url: "http://localhost:3000/users/auth/facebook/callback"
   config.omniauth :github, ENV['CLIENT_ID'], ENV['APP_SECRET'], :scope => 'user:email'
 
