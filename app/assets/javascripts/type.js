92051916edb2f7489eda40cca2818e11ebfc6c2e
JingruZhang <zhang.jingru.93@gmail.com>
2018-02-05 14:16:45 -0800

    grab the next type via AJAX

--- app/assets/javascripts/type.js
+++ app/assets/javascripts/type.js
@@ -13,10 +13,17 @@ function nextTypeListener() {
     $('#js-next-type').click(function(ele) {
     	ele.preventDefault();
     	var id = getCurrentTypeId();
+    	var next_id = null;
     	$.getJSON('/types', function(data) {
-    		
+    		$.each(data, function( key, val ) { 
+    			if (val.id === id) {
+    				next_id = data[key + 1].id;
+    			}
+    		})
+    		$.getJSON('/types/' + next_id).success()
     	})
     })
+
 }
 
 //previous type listener
