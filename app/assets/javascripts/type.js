58ebeca1d9d55192c6754d30ae9b4db0a99de492
JingruZhang <zhang.jingru.93@gmail.com>
2018-02-06 16:13:13 -0800

    render json data for type using template on type/show

--- app/assets/javascripts/type.js
+++ app/assets/javascripts/type.js
@@ -1,3 +1,13 @@
+class Type {
+	constructor(type_params) {
+		this.id = type_params.id;
+		this.name = type_params.name;
+		this.about = type_params.about;
+		this.instruction = type_params.instruction;
+		this.teas = type_params.teas;
+	}
+}
+
 $(function() {
 	nextTypeListener(); 
 	previousTypeListener()
@@ -12,19 +22,31 @@ function getCurrentTypeId() {
 function nextTypeListener() {
     $('#js-next-type').click(function(ele) {
     	ele.preventDefault();
-    	var id = getCurrentTypeId();
-    	var next_id = null;
-    	$.getJSON('/types', function(data) {
-    		$.each(data, function( key, val ) { 
-    			if (val.id === id) {
-    				next_id = data[key + 1].id;
-    			}
-    		})
-    		$.getJSON('/types/' + next_id).success()
-    	})
+    	let id = getCurrentTypeId();
+    	let next_id = null;
+    	loadType(id, next_id);
     })
 
 }
 
 //previous type listener
 function previousTypeListener() {}
+
+//load type
+function loadType (id, next_id) {
+	$.getJSON('/types', function(data) {
+		$.each(data, function( key, val ) { 
+			if (val.id === id) {
+				next_id = data[key + 1].id;
+			}
+		})
+		$.getJSON('/types/' + next_id).done(function(data) {
+			let next_type = new Type(data);
+			let next_type_show = $(HandlebarsTemplates['types/show-type'](next_type));
+			let next_type_tea = $(HandlebarsTemplates['types/show-type-tea'](next_type))
+			$('#clicked-tea').empty();
+			$('#js-type').html(next_type_show);
+			$('#js-type-tea').html(next_type_tea);
+		})
+	})
+}
