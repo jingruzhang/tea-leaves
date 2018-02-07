580eb67805b54eef7e33861150a9eb69e3c82d3e
JingruZhang <zhang.jingru.93@gmail.com>
2018-02-07 14:45:54 -0800

    refactor for json and js model objects

--- app/assets/javascripts/tea.js
+++ app/assets/javascripts/tea.js
@@ -1,11 +1,33 @@
-$(function() {
+$(
+	teaClickListener();
+)
+
+class Tea {
+	constructor(tea_params) {
+		this.id = tea_params.id;
+		this.name = tea_params.name;
+		this.origin = tea_params.origin;
+		this.profile = tea_params.profile;
+		this.instruction = tea_params.instruction;
+		this.type_id = tea_params.type_id;
+		this.reviews = tea_params.reviews;
+	}
+}
+
+//Listener function for click
+function teaClickListener() {
 	Array.from($('.tea')).forEach(function(ele){ 
 		var uri = $(ele).attr('href');
 		ele.addEventListener("click", function(elem) { 
 			elem.preventDefault();
-			$.get(uri, function(data) {
-				$('#clicked-tea').html(data)
-			})
+			loadAjax(uri);
 		})
 	})
-})
+}
+
+//load ajax
+function loadAjax(uri) {
+	$.get(uri, function(data) {
+		$('#clicked-tea').html(data)
+	})
+}
