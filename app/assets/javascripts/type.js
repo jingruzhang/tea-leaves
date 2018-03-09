77f3b4e014265335276470f755b534438f056ad0
JingruZhang <zhang.jingru.93@gmail.com>
2018-03-09 04:45:05 -0800

    fix add tea button not reflecting new type

--- app/assets/javascripts/type.js
+++ app/assets/javascripts/type.js
@@ -50,26 +50,35 @@ function previousTypeListener() {
 
 //load type
 function loadType (newId) {
-	$.getJSON('/types', function(data) {
-		$.getJSON('/types/' + newId).done(function(data) {
-			let nextType = new Type(data);
-			nextType.teas.sort(function(teaA, teaB) {
-				let nameA = teaA.name.toUpperCase();
-				let nameB = teaB.name.toUpperCase();
-				if (nameA < nameB) {
-					return -1;
-				} if (nameA > nameB) {
-					return 1;
-				} else {
-					return 0;
-				}
-			})
-			let nextTypeShow = $(HandlebarsTemplates['types/show-type'](nextType));
-			let nextTypeTea = $(HandlebarsTemplates['types/show-type-tea'](nextType));
-			$('#clicked-tea').empty();
-			$('#js-type').html(nextTypeShow);
-			$('#js-type-tea').html(nextTypeTea);
-			history.pushState(null, null, '/types/' + nextType.id)
+	$.getJSON('/types/' + newId).done(function(data) {
+		let nextType = new Type(data);
+		nextType.teas.sort(function(teaA, teaB) {
+			let nameA = teaA.name.toUpperCase();
+			let nameB = teaB.name.toUpperCase();
+			if (nameA < nameB) {
+				return -1;
+			} if (nameA > nameB) {
+				return 1;
+			} else {
+				return 0;
+			}
 		})
+		let nextTypeShow = $(HandlebarsTemplates['types/show-type'](nextType));
+		let nextTypeTea = $(HandlebarsTemplates['types/show-type-tea'](nextType));
+		$('#clicked-tea').empty();
+		$('#js-type').html(nextTypeShow);
+		$('#js-type-tea').html(nextTypeTea);
+		renewButton(nextType)
+		history.pushState(null, null, '/types/' + nextType.id)
 	})
+}
+
+//renew type buttons
+function renewButton(type) {
+	let buttonString = currentUser != null ? 
+		"<p>Didn't see what you're looking for?</p>" +
+		`<p><a class='btn btn-primary' href='/types/${type.id}/teas/new' role='button'>Add a new ${type.name} tea</a></p>`	
+		: "<br>" + 
+		"<p><a class='btn btn-primary' href='/users/sign_in' role='button' id='add-review'>Sign in to add tea and reviews!</a></p>"
+	$('#js-type-buttons').html(buttonString)
 }
